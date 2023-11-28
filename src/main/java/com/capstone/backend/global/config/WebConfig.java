f4cbb850e8d5916710223fc5685eb48769fbe480
한민정 <zoangrak@gmail.com>
2023-11-29 01:07:58 +0900

    authorization 헤더를 클라이언트에게 노출

--- src/main/java/com/capstone/backend/global/config/WebConfig.java
+++ src/main/java/com/capstone/backend/global/config/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
     }
 }
