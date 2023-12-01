7237b44841b53ea2c8b9634f57089c3938464967
한민정 <zoangrak@gmail.com>
2023-12-01 20:12:13 +0900

    jackson 설정

--- src/main/java/com/capstone/backend/domain/chat/dto/ChatDto.java
+++ src/main/java/com/capstone/backend/domain/chat/dto/ChatDto.java
@@ -1,10 +1,15 @@
 package com.capstone.backend.domain.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class ChatDto {
     public enum MessageType {
         ENTER, CHAT, LEAVE
@@ -15,4 +20,19 @@ public class ChatDto {
     private String sender; // 채팅을 보낸 사람
     private String message; // 채팅 메세지
     private String time; // 채팅 발송 시간
+
+    @JsonCreator
+    public ChatDto(@JsonProperty("type") MessageType type,
+                   @JsonProperty("hidden") int[] hidden,
+                   @JsonProperty("roomId") String roomId,
+                   @JsonProperty("sender") String sender,
+                   @JsonProperty("message") String message,
+                   @JsonProperty("time") String time) {
+        this.type = type;
+        this.hidden = hidden;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.message = message;
+        this.time = time;
+    }
 }
